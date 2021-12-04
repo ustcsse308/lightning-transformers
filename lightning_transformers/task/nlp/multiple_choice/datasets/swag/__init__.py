b8a5c32c4898a48bf627e69a50d6d2ed97600448
Zac Bi <zacb2018@mail.ustc.edu.cn>
2021-12-04 15:37:11 +0800

    refactor: :coffin: 删除无用任务

--- lightning_transformers/task/nlp/multiple_choice/datasets/swag/__init__.py
+++
@@ -1,3 +0,0 @@
-from lightning_transformers.task.nlp.multiple_choice.datasets.swag.data import (  # noqa: F401
-    SwagMultipleChoiceDataModule,
-)
--- lightning_transformers/task/nlp/token_classification/__init__.py
+++
@@ -1,2 +0,0 @@
-from lightning_transformers.task.nlp.token_classification.data import TokenClassificationDataModule  # noqa: F401
-from lightning_transformers.task.nlp.token_classification.model import TokenClassificationTransformer  # noqa: F401
